73f002d0507c112e22dee7b8f2e231a6143005ef
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-24 22:50:14 +0300

    Add PrototypePattern Schema

---
+++ creational-patterns/prototype-pattern/Schema/PrototypePatternSchema.ts
@@ -0,0 +1,46 @@
+class Prototype {
+    private primitiveData: any;
+    private component: object;
+    private circularReference: ComponentWithBackReference;
+
+    public setPrimitiveData = (newPrimitiveData: any) => {
+        this.primitiveData = newPrimitiveData;
+    }
+
+    public setComponent = (newComponent: object) => {
+        this.component = newComponent;
+    }
+
+    public setCircularReference = (newCircularReference: ComponentWithBackReference) => {
+        this.circularReference = newCircularReference;
+    }
+
+    public clone = (): this => {
+        const clone = Object.create(this);
+
+        clone.component = Object.create(this.component);
+        clone.circularReference = {
+          ...this.circularReference,
+          prototype: { ...this },
+        };
+
+        return clone;
+    }
+}
+
+class ComponentWithBackReference {
+    public prototype: Prototype;
+
+    constructor(prototype: Prototype) {
+        this.prototype = prototype;
+    }
+}
+
+const main = () => {
+    const prototype = new Prototype();
+    prototype.setPrimitiveData(1234);
+    prototype.setComponent(new Date());
+    prototype.setCircularReference(new ComponentWithBackReference(prototype));
+
+    const prototypeClone = prototype.clone();
+}
