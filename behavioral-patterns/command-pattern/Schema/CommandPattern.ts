08e7a1ae76888e9c73dd55cf9bd72a1fbc692dcf
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-05-23 00:34:56 +0300

    Set Schema for command pattern

---
+++ behavioral-patterns/command-pattern/Schema/CommandPattern.ts
@@ -0,0 +1,45 @@
+interface Command {
+    execute(): void;
+}
+
+class SimpleCommand implements Command {
+    execute(): void {
+    }
+}
+
+class ComplexCommand implements Command {
+    execute(): void {
+    }
+}
+
+class Invoker {
+    private onStart: Command;
+    private onFinish: Command;
+
+    public setOnStart(command: Command) {
+        this.onStart = command;
+    }
+
+    public setOnFinish(command: Command) {
+        this.onFinish = command;
+    }
+
+    private static isCommand(object): object is Command {
+        return object.execute != undefined;
+    }
+
+    public performSomething(): void {
+        if (Invoker.isCommand(this.onStart)) {
+            this.onStart.execute();
+        }
+
+        if (Invoker.isCommand(this.onFinish)) {
+            this.onFinish.execute();
+        }
+    }
+}
+
+const invoker = new Invoker();
+invoker.setOnStart(new SimpleCommand());
+invoker.setOnFinish(new SimpleCommand());
+invoker.performSomething();
